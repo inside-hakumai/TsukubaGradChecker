124dcbd03acb9c7a1a436944f37994d4d44a1914
Hakumai <inside.rice164@gmail.com>
2017-01-17 13:44:54 +0900

    セキュリティに関わる記述

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var helmet = require('helmet');
+var csrf = require('lusca').csrf();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// configration for security
+app.use(helmet());
+app.use(lusca.csrf());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,20 +33,20 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+   var err = new Error('Not Found');
+   err.status = 404;
+   next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+   // set locals, only providing error in development
+   res.locals.message = err.message;
+   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+   // render the error page
+   res.status(err.status || 500);
+   res.render('error');
 });
 
 module.exports = app;
