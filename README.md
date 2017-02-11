8dfabe375eb07b9c79142ffd8dddc5f444474654
Hakumai <inside.rice164@gmail.com>
2017-02-12 01:43:16 +0900

    remove existing files

--- README.md
+++
@@ -1,7 +0,0 @@
-# requirement-verificator(卒業要件ちぇっかー)
-卒業要件と取得単位の情報を照合して結果を表示するサービスです。
-
---> http://checker.insidehakumai.net
-
-## 自分の取得単位データを使わず使ってみる
-取得単位データのサンプル([sample/sample_grade.csv](/sample/sample_grade.csv))を使用してください。
--- public/src/jsx/script.jsx
+++
@@ -1,8 +0,0 @@
-/* global $ */ // avoid ESLint warning
-
-$(function() {
-   $('div.mdl-select > ul > li').click(function(e) {
-      var text = $(e.target).text();
-      $(e.target).parents('.mdl-select').addClass('is-dirty').children('input').val(text);
-   });
-});
