ce04cd23846b38532493317fe5c6a6be613abe94
Hakumai <inside.rice164@gmail.com>
2017-03-29 13:53:36 +0900

    set header to fixed position and set the opacity

--- public/src/jsx/script.jsx
+++ public/src/jsx/script.jsx
@@ -1,21 +1,41 @@
 /* global $ */ // avoid ESLint warning
 
-$(function() {
-   window.onload = function(){
-      $('body').css('display', 'block');
-   };
+let window_height;
 
+$(document).ready(function(){
+   $(window).resize(resizeTitleArea);
+   $('main.mdl-layout__content').scroll(changeHeaderColorIfNeeded);
+   resizeTitleArea();
+   changeHeaderColorIfNeeded();
    $('div.mdl-select > ul > li').click(function(e) {
       let text = $(e.target).text();
       //noinspection JSUnresolvedFunction
-       $(e.target).parents('.mdl-select').addClass('is-dirty').children('input').val(text);
+      $(e.target).parents('.mdl-select').addClass('is-dirty').children('input').val(text);
    });
-   resizeTitleArea();
-
-   $(window).resize(resizeTitleArea);
 });
 
-function resizeTitleArea() {
-   let height = $(window).height() - $('header').height();
-   $('#title').css('height', height + 'px');
+$(window).load(function(){
+   $('body').css('display', 'block');
+});
+
+function resizeTitleArea(e) {
+   console.log(e);
+   window_height = $(window).height();
+   $('#title').css('height', window_height + 'px');
+}
+
+function changeHeaderColorIfNeeded(){
+   if(changeHeaderColorIfNeeded.onTitleArea === undefined){
+      changeHeaderColorIfNeeded.onTitleArea = !($('main.mdl-layout__content').scrollTop() < window_height);
+   }
+
+   let onTitleAreaNow = $('main.mdl-layout__content').scrollTop() < window_height;
+
+   if (onTitleAreaNow && !changeHeaderColorIfNeeded.onTitleArea){
+      $('header.mdl-layout__header.mdl-layout__header--transparent.is-casting-shadow').css('background-color', 'transparent');
+      changeHeaderColorIfNeeded.onTitleArea = true;
+   } else if(!onTitleAreaNow && changeHeaderColorIfNeeded.onTitleArea) {
+      $('header.mdl-layout__header.mdl-layout__header--transparent.is-casting-shadow').css('background-color', 'rgba(0, 0, 0, 0.65)');
+      changeHeaderColorIfNeeded.onTitleArea = false;
+   }
 }
